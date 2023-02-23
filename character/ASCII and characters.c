6c2aa012b194b9f065d6530c9842f92f4ffc0e33
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-02-23 07:46:35 +0530

    Add files via upload

---
+++ character/ASCII and characters.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+// int main(){
+//     char ch = 'a';
+//     printf("%c", ch);
+//     return 0;
+// }
+
+//ASCII Values
+
+int main(){
+    char a = 'A'; printf("%d", a);
+    return 0;
+}
+/*
+A=65
+B=66
+C=67
+D=68....
+a=97
+b=98
+c=99
+d=100....
+!=33
+@=64
+#=35
+$=36
+*=43....
+1=49
+2=50
+3=52
+4=53....
+*/
