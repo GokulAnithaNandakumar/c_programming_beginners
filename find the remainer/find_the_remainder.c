cb69bdd77d24ceb466582447af648818bb1d5bf0
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-02-25 18:27:18 +0530

    Add files via upload

---
+++ find the remainer/find_the_remainder.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter the divident: ");
+    scanf("%d", &a);
+    printf("Enter the divisor: ");
+    scanf("%d", &b);
+    printf("%d", a%b);
+    return 0;
+}
