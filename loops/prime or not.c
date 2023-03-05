c6518a3779d03c8175011937aa30266ad21ec89f
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-05 09:49:08 +0530

    Update prime or not.c

--- loops/prime or not.c
+++ loops/prime or not.c
@@ -1,34 +1,31 @@
 //
 //  prime or not.c
 //  loops
-//
-//  Created by Gokul Nandakumar on 19/02/23.
-//
 
-//#include<stdio.h>
-//int main(){
-//    int n;
-//    int a = 1;
-//    printf("Enter the number: ");
-//    scanf("%d", &n);
-//    if (n==1) printf("1 is neither prime nor composite");
-//    else{
-//        for (int i=2; i<(n/2); i++) {
-//            if(n%i==0){
-//                a = 0;
-//                break;
-//
-//            }
-//            else{
-//                a = 1;
-//            }
-//        }
-//        if(a==0){
-//            printf("Not Prime");
-//        }
-//        else{
-//            printf("Prime");
-//        }
-//
-//    }
-//}
+#include<stdio.h>
+int main(){
+   int n;
+   int a = 1;
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   if (n==1) printf("1 is neither prime nor composite");
+   else{
+       for (int i=2; i<(n/2); i++) {
+           if(n%i==0){
+               a = 0;
+               break;
+
+           }
+           else{
+               a = 1;
+           }
+       }
+       if(a==0){
+           printf("Not Prime");
+       }
+       else{
+           printf("Prime");
+       }
+
+   }
+}
