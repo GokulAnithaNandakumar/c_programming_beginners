8d683786e86c9c355cd191e3fd44b75fa9cd9e79
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-05 09:47:48 +0530

    Add files via upload

---
+++ loops/fibonocci.c
@@ -0,0 +1,52 @@
+//
+//  fibonocci.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 19/02/23.
+//
+
+#include <stdio.h>
+//using while loop
+//int main(){
+//    int n;
+//    int a=0;
+//    int b=1;
+//    int fib;
+//    int cout = 0;
+//    printf("Enter the term: ");
+//    scanf("%d", &n);
+//    if(n<=0){printf("Enter a positive number");}
+//    else if (n==1){printf("%d", a);}
+//    else if (n==2){printf("%d", b);}
+//    else{
+//        while (cout < (n-2)) {
+//
+//            fib=a+b;
+//
+//            a=b;
+//            b=fib;
+//
+//            cout++;
+//        }printf("%d",fib);
+//    }
+//}
+
+
+//using for loop
+
+//int main(){
+//    int n;
+//    int a=0;
+//    int b=1;
+//    int fib;
+//
+//    printf("Enter the term: ");
+//    scanf("%d", &n);
+//    for (int i=1; i<=n-2; i++) {
+//        fib=a+b;
+//        a=b;
+//        b=fib;
+//
+//    }printf("%d", fib);
+//}
+//
