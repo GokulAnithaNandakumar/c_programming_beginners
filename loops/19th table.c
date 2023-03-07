09756d3700ddf29a78474ad0aa6a0391261718d0
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-07 15:00:20 +0530

    Update 19th table.c

--- loops/19th table.c
+++ loops/19th table.c
@@ -1,13 +1,9 @@
-//
 //  19th table.c
 //  loops
-//
-//  Created by Gokul Nandakumar on 18/02/23.
-//
 
-//#include <stdio.h>
-//int main(){
-//    for (int i=19; i<=190; i=i+19) {
-//        printf("%d\n", i);
-//    }
-//}
+#include <stdio.h>
+int main(){
+   for (int i=19; i<=190; i=i+19) {
+       printf("%d\n", i);
+   }
+}
