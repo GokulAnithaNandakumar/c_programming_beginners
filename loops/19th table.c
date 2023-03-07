c2df562edf7b1aabab8107dfbf73f44285e2ce07
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-07 14:59:40 +0530

    Add files via upload

---
+++ loops/19th table.c
@@ -0,0 +1,13 @@
+//
+//  19th table.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 18/02/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    for (int i=19; i<=190; i=i+19) {
+//        printf("%d\n", i);
+//    }
+//}
