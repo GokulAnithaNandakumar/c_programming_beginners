41e6c4a850d2895baedb9daf604dfadd7a8a0345
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-08 07:28:23 +0530

    Add files via upload

---
+++ loops/digits of number.c
@@ -0,0 +1,23 @@
+//
+//  digits of number.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 19/02/23.
+//
+
+
+//my code
+//#include <stdio.h>
+//int main(){
+//    int n;
+//    int x = 0;
+//
+//    printf("Enter the number: ");
+//    scanf("%d", &n);
+//    while(n!=0){
+//        n=n/10;
+//        x++;
+//    }
+//    printf("%d", x);
+//}
+
