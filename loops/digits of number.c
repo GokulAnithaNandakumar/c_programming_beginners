04369f70af85407417f3ac9fd038d9a98a307957
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-08 07:29:04 +0530

    Update digits of number.c

--- loops/digits of number.c
+++ loops/digits of number.c
@@ -1,23 +1,17 @@
-//
+
 //  digits of number.c
-//  loops
-//
-//  Created by Gokul Nandakumar on 19/02/23.
-//
 
+#include <stdio.h>
+int main(){
+   int n;
+   int x = 0;
 
-//my code
-//#include <stdio.h>
-//int main(){
-//    int n;
-//    int x = 0;
-//
-//    printf("Enter the number: ");
-//    scanf("%d", &n);
-//    while(n!=0){
-//        n=n/10;
-//        x++;
-//    }
-//    printf("%d", x);
-//}
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   while(n!=0){
+       n=n/10;
+       x++;
+   }
+   printf("%d", x);
+}
 
