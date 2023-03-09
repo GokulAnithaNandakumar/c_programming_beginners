716149467ff5962324b1756dba283ac9bb8d2abf
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-09 05:33:46 +0530

    Update tables math.c

--- loops/tables math.c
+++ loops/tables math.c
@@ -2,15 +2,13 @@
 //  tables math.c
 //  loops
 //
-//  Created by Gokul Nandakumar on 18/02/23.
-//
 
-//#include<stdio.h>
-//int main(){
-//    int n;
-//    printf("Enter the number: ");
-//    scanf("%d", &n);
-//    for (int i=1; i<=10; i++) {
-//        printf("%d\n", n*i);
-//    }
-//}
+#include<stdio.h>
+int main(){
+   int n;
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   for (int i=1; i<=10; i++) {
+       printf("%d\n", n*i);
+   }
+}
