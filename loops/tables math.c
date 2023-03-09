c70043bdcf76e41cf63b7c0c31972aca4c65d303
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-09 05:33:15 +0530

    Add files via upload

---
+++ loops/tables math.c
@@ -0,0 +1,16 @@
+//
+//  tables math.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 18/02/23.
+//
+
+//#include<stdio.h>
+//int main(){
+//    int n;
+//    printf("Enter the number: ");
+//    scanf("%d", &n);
+//    for (int i=1; i<=10; i++) {
+//        printf("%d\n", n*i);
+//    }
+//}
