5b33e49358e47d2a4d5b2b59580f096fa82691a0
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-10 09:11:38 +0530

    Update AP_without_math.c

--- loops/AP_without_math.c
+++ loops/AP_without_math.c
@@ -1,21 +1,17 @@
 //
 //  AP_without_math.c
 //  loops
-//
-//  Created by Gokul Nandakumar on 19/02/23.
-//
 
-
-//#include<stdio.h>
-//int main(){
-//    int n;
-//    printf("Enter the number: ");
-//    scanf("%d", &n);
-//    //4,7,10,13,16,19... upto n terms
-//    int a = 4;
-//    for (int i=1; i<=n; i++) {
-//        printf("%d\n", a);
-//        a = a + 3;
-//    }
-//}
+#include<stdio.h>
+int main(){
+   int n;
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   //4,7,10,13,16,19... upto n terms
+   int a = 4;
+   for (int i=1; i<=n; i++) {
+       printf("%d\n", a);
+       a = a + 3;
+   }
+}
 
