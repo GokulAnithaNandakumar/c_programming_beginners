8eb510ac0826e35da92f35e04972e15e5629d1eb
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-11 09:07:27 +0530

    Update even number from 1 to 100.c

--- loops/even number from 1 to 100.c
+++ loops/even number from 1 to 100.c
@@ -1,33 +1,30 @@
 //
 //  even number from 1 to 100.c
 //  loops
-//
-//  Created by Gokul Nandakumar on 18/02/23.
-//
 
 #include <stdio.h>
-//int main(){
-//    for(int i=1; i<=100; i++){
-//        if (i%2==0){
-//            printf("%d", i);
-//        }
-//        else{
-//            printf("\n");
-//        }
-//    }
-//}
-//int main(){
-//    for(int i=1; i<=100; i++){
-//        if (i%2==0){
-//            printf("%d ", i);
-//        }
-//
-//    }
-//}
-//int main(){
-//    for(int i=1; i<=100; i++){
-//        if (i%2==0) printf("%d ", i);
-//        
-//        
-//    }
-//}
+int main(){
+   for(int i=1; i<=100; i++){
+       if (i%2==0){
+           printf("%d", i);
+       }
+       else{
+           printf("\n");
+       }
+   }
+}
+int main(){
+   for(int i=1; i<=100; i++){
+       if (i%2==0){
+           printf("%d ", i);
+       }
+
+   }
+}
+int main(){
+   for(int i=1; i<=100; i++){
+       if (i%2==0) printf("%d ", i);
+       
+       
+   }
+}
