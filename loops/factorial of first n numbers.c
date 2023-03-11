893b79198278790fc7b3fedfb684a30c22f84b4f
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-12 00:30:45 +0530

    Update factorial of first n numbers.c

--- loops/factorial of first n numbers.c
+++ loops/factorial of first n numbers.c
@@ -6,38 +6,38 @@
 //
 
 #include <stdio.h>
-//int factorial(int n){
-//
-//    int x=1;
-//
-//    if (n==0){printf("0");}
-//
-//    else{
-//        for (int i=n; i>=1; i--) {
-//            x=x*i;
-//        }
-//
-//        printf("%d\n", x);
-//    }return 0;
-//}
-//
-//int main(){
-//    int n;
-//    printf("Enter the number: ");
-//    scanf("%d", &n);
-//    for (int i=1; i<=n; i++) {
-//        factorial(i);
-//    }
-//}
-//sir code
-//int main(){
-//    int n;
-//    printf("Enter the number: ");
-//    scanf("%d", &n);
-//    int prod=1;
-//    for (int i=1; i<=n; i++) {
-//        prod=prod*i;
-//        printf("The factorial of %d is: %d\n", i, prod);
-//    }
-//
-//}
+int factorial(int n){
+
+   int x=1;
+
+   if (n==0){printf("0");}
+
+   else{
+       for (int i=n; i>=1; i--) {
+           x=x*i;
+       }
+
+       printf("%d\n", x);
+   }return 0;
+}
+
+int main(){
+   int n;
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   for (int i=1; i<=n; i++) {
+       factorial(i);
+   }
+}
+sir code
+int main(){
+   int n;
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   int prod=1;
+   for (int i=1; i<=n; i++) {
+       prod=prod*i;
+       printf("The factorial of %d is: %d\n", i, prod);
+   }
+
+}
