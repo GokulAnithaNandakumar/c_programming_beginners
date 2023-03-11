1563abe8d1846c1af7b2bf8ff859474ef4b11a0b
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-12 00:30:21 +0530

    Add files via upload

---
+++ loops/factorial of first n numbers.c
@@ -0,0 +1,43 @@
+//
+//  factorial of first n numbers.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 19/02/23.
+//
+
+#include <stdio.h>
+//int factorial(int n){
+//
+//    int x=1;
+//
+//    if (n==0){printf("0");}
+//
+//    else{
+//        for (int i=n; i>=1; i--) {
+//            x=x*i;
+//        }
+//
+//        printf("%d\n", x);
+//    }return 0;
+//}
+//
+//int main(){
+//    int n;
+//    printf("Enter the number: ");
+//    scanf("%d", &n);
+//    for (int i=1; i<=n; i++) {
+//        factorial(i);
+//    }
+//}
+//sir code
+//int main(){
+//    int n;
+//    printf("Enter the number: ");
+//    scanf("%d", &n);
+//    int prod=1;
+//    for (int i=1; i<=n; i++) {
+//        prod=prod*i;
+//        printf("The factorial of %d is: %d\n", i, prod);
+//    }
+//
+//}
