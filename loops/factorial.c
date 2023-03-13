c34fe8f06c8ae5bbe5f286582079190497ab1375
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-13 08:59:48 +0530

    Update factorial.c

--- loops/factorial.c
+++ loops/factorial.c
@@ -5,19 +5,19 @@
 //  Created by Gokul Nandakumar on 19/02/23.
 //
 
-//#include <stdio.h>
-//int main(){
-//    int n;
-//    int x=1;
-//    printf("Enter the number: ");
-//    scanf("%d", &n);
-//    if (n==0){printf("0");}
-//
-//    else{
-//        for (int i=n; i>=1; i--) {
-//            x=x*i;
-//        }
-//        
-//        printf("%d", x);
-//    }
-//}
+#include <stdio.h>
+int main(){
+   int n;
+   int x=1;
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   if (n==0){printf("0");}
+
+   else{
+       for (int i=n; i>=1; i--) {
+           x=x*i;
+       }
+       
+       printf("%d", x);
+   }
+}
