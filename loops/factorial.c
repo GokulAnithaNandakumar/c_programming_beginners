1216e680ed4cc02af5fb8d744cc2329916ca5dd3
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-13 08:59:24 +0530

    Add files via upload

---
+++ loops/factorial.c
@@ -0,0 +1,23 @@
+//
+//  factorial.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 19/02/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    int n;
+//    int x=1;
+//    printf("Enter the number: ");
+//    scanf("%d", &n);
+//    if (n==0){printf("0");}
+//
+//    else{
+//        for (int i=n; i>=1; i--) {
+//            x=x*i;
+//        }
+//        
+//        printf("%d", x);
+//    }
+//}
