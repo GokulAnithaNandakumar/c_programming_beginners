4e0c17ba9231a27ffcdecd70c78608897287b9e3
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-14 14:46:28 +0530

    Add files via upload

---
+++ loops/first n fibonocci numbers.c
@@ -0,0 +1,27 @@
+//
+//  first n fibonocci numbers.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 19/02/23.
+//
+
+//#include "first n fibonocci numbers.h"
+//int main(){
+//    int n;
+//    int a=0;
+//    int b=1;
+//    int fib;
+//
+//    printf("Enter the term: ");
+//    scanf("%d", &n);
+//    printf("The 1 fibonocci number is 1\n");
+//    printf("The 2 fibonocci number is 1\n");
+//    for (int i=1; i<=n-2; i++) {
+//        fib=a+b;
+//        a=b;
+//        b=fib;
+//        printf("The %d fibonocci number is %d\n", i+2, fib);
+//
+//    }
+//}
+
