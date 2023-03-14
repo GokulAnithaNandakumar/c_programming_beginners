6b4efc1dd99d2328ee5349cd4b2fb5b84f34e096
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-15 00:23:16 +0530

    Update power.c

--- loops/power.c
+++ loops/power.c
@@ -1,19 +1,16 @@
 //
 //  power.c
 //  loops
-//
-//  Created by Gokul Nandakumar on 19/02/23.
-//
 
 #include <stdio.h>
-//int main(){
-//    int a,b;
-//    printf("Enter the base: ");
-//    scanf("%d", &a);
-//    printf("Enter the exponent: ");
-//    scanf("%d", &b);
-//    int ans=1;
-//    for (int i=1; i<=b; i++) {
-//        ans=ans*a;
-//    }printf("%d", ans);
-//}
+int main(){
+   int a,b;
+   printf("Enter the base: ");
+   scanf("%d", &a);
+   printf("Enter the exponent: ");
+   scanf("%d", &b);
+   int ans=1;
+   for (int i=1; i<=b; i++) {
+       ans=ans*a;
+   }printf("%d", ans);
+}
