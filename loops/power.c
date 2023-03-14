c578a58020ff6c8e6f8a9832c6d621cbbb7c4b30
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-15 00:22:52 +0530

    Add files via upload

---
+++ loops/power.c
@@ -0,0 +1,19 @@
+//
+//  power.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 19/02/23.
+//
+
+#include <stdio.h>
+//int main(){
+//    int a,b;
+//    printf("Enter the base: ");
+//    scanf("%d", &a);
+//    printf("Enter the exponent: ");
+//    scanf("%d", &b);
+//    int ans=1;
+//    for (int i=1; i<=b; i++) {
+//        ans=ans*a;
+//    }printf("%d", ans);
+//}
