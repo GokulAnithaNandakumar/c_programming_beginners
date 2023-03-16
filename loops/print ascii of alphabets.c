4d5929892d3b9fe5d24b61409f79bd5b49b0e418
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-16 12:15:44 +0530

    Update print ascii of alphabets.c

--- loops/print ascii of alphabets.c
+++ loops/print ascii of alphabets.c
@@ -1,20 +1,17 @@
 //
 //  print ascii.c
 //  loops
-//
-//  Created by Gokul Nandakumar on 19/02/23.
-//
 
-//#include <stdio.h>
-//int main(){
-//    int x=65;
-//    printf("%d\t", x);
-//    char a = (char)x;
-//    printf("%c\n", a);
-//    for (int i=65; i<=90; i++) {
-//        //typecasting
-//        char ch = (char)i;
-//        printf("%c --> ", ch);
-//        printf("%d\n",i);
-//    }
-//}
+#include <stdio.h>
+int main(){
+   int x=65;
+   printf("%d\t", x);
+   char a = (char)x;
+   printf("%c\n", a);
+   for (int i=65; i<=90; i++) {
+       //typecasting
+       char ch = (char)i;
+       printf("%c --> ", ch);
+       printf("%d\n",i);
+   }
+}
