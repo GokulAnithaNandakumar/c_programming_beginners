ba0fa41fab809e0baa334d5f879dd2a69f40bd4a
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-16 12:15:07 +0530

    Add files via upload

---
+++ loops/print ascii of alphabets.c
@@ -0,0 +1,20 @@
+//
+//  print ascii.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 19/02/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    int x=65;
+//    printf("%d\t", x);
+//    char a = (char)x;
+//    printf("%c\n", a);
+//    for (int i=65; i<=90; i++) {
+//        //typecasting
+//        char ch = (char)i;
+//        printf("%c --> ", ch);
+//        printf("%d\n",i);
+//    }
+//}
