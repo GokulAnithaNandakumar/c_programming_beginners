40a5766dd2c4db9605b5bd9ae67b6142a49a2b6c
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-17 09:17:46 +0530

    Update sum of series.c

--- loops/sum of series.c
+++ loops/sum of series.c
@@ -1,27 +1,24 @@
 //
 //  sum of series.c
 //  loops
-//
-//  Created by Gokul Nandakumar on 19/02/23.
-//
-//
-//#include <stdio.h>
-////1-2+3-4+5-6.... unto n terms
-//int main(){
-//    int n;
-//    printf("Enter the number of terms: ");
-//    scanf("%d", &n);
-//    int x=1;
-//    for (int i=2; n!=1; i++) {
-//        if (n%2==0){
-//            x=x-i;
-//            n--;
-//        }
-//        else{
-//            x=x+i;
-//            n--;
-//        }
-//    }
-//
-//    printf("%d", x);
-//}
+
+#include <stdio.h>
+//1-2+3-4+5-6.... unto n terms
+int main(){
+   int n;
+   printf("Enter the number of terms: ");
+   scanf("%d", &n);
+   int x=1;
+   for (int i=2; n!=1; i++) {
+       if (n%2==0){
+           x=x-i;
+           n--;
+       }
+       else{
+           x=x+i;
+           n--;
+       }
+   }
+
+   printf("%d", x);
+}
