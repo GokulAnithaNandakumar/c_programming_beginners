1371a687176d296000294d1861674c9e9b944fec
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-19 12:18:07 +0530

    Update sum of even digits.c

--- loops/sum of even digits.c
+++ loops/sum of even digits.c
@@ -1,27 +1,24 @@
 //
 //  sum of even digits.c
 //  loops
-//
-//  Created by Gokul Nandakumar on 19/02/23.
-//
 
-//#include <stdio.h>
-//int main(){
-//    int n;
-//    int x = 0;
-////    int y;
-//
-//    printf("Enter the number: ");
-//    scanf("%d", &n);
-//    while (n!=0){
-//        if ((n%10)%2==0){
-//            x=x+(n%10);
-//            n=n/10;
-//        }
-//        else{
-//            n=n/10;
-//        }
-//
-//    }
-//    printf("%d", x);
-//}
+#include <stdio.h>
+int main(){
+   int n;
+   int x = 0;
+//    int y;
+
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   while (n!=0){
+       if ((n%10)%2==0){
+           x=x+(n%10);
+           n=n/10;
+       }
+       else{
+           n=n/10;
+       }
+
+   }
+   printf("%d", x);
+}
