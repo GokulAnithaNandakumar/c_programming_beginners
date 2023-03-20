fafe8bf297856d4ab1a427a7e43a9e4c34fa1dd4
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-20 16:14:12 +0530

    Add files via upload

---
+++ loops/sum of digits.c
@@ -0,0 +1,23 @@
+//
+//  sum of digits.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 19/02/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    int n;
+//    int x = 0;
+//    int y;
+//    
+//    printf("Enter the number: ");
+//    scanf("%d", &n);
+//    while (n!=0){
+//        y=n%10;
+//        x=x+y;
+//        n=n/10;
+//        
+//    }
+//    printf("%d", x)
+//}
