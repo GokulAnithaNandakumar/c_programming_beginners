a5986cf782ea161f8795116d34a2f396cb544e06
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-21 13:52:06 +0530

    Update reverse of a number.c

--- loops/reverse of a number.c
+++ loops/reverse of a number.c
@@ -1,39 +1,36 @@
 //
 //  reverse of a number.c
 //  loops
-//
-//  Created by Gokul Nandakumar on 19/02/23.
-//
 
 #include <stdio.h>
-//int main(){
-//    int n;
-//    int y;
-//
-//    printf("Enter the number: ");
-//    scanf("%d", &n);
-//    while (n!=0){
-//        y=n%10;
-//        printf("%d", y);
-//        n=n/10;
-//    }
-//
-//}
+int main(){
+   int n;
+   int y;
 
-//int main(){
-//    int n;
-//    int r=0;
-//    
-//    printf("Enter the number: ");
-//    scanf("%d", &n);
-//    while (n>0){
-//        r=r*10;
-//        r = r+(n%10);
-//        
-//        
-//        n=n/10;
-//    
-//    }
-//    printf("%d", r);
-//
-//}
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   while (n!=0){
+       y=n%10;
+       printf("%d", y);
+       n=n/10;
+   }
+
+}
+
+int main(){
+   int n;
+   int r=0;
+   
+   printf("Enter the number: ");
+   scanf("%d", &n);
+   while (n>0){
+       r=r*10;
+       r = r+(n%10);
+       
+       
+       n=n/10;
+   
+   }
+   printf("%d", r);
+
+}
