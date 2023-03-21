74694ca8aa851f71a0a71956e00ade300b3a99fe
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-21 13:51:26 +0530

    Add files via upload

---
+++ loops/reverse of a number.c
@@ -0,0 +1,39 @@
+//
+//  reverse of a number.c
+//  loops
+//
+//  Created by Gokul Nandakumar on 19/02/23.
+//
+
+#include <stdio.h>
+//int main(){
+//    int n;
+//    int y;
+//
+//    printf("Enter the number: ");
+//    scanf("%d", &n);
+//    while (n!=0){
+//        y=n%10;
+//        printf("%d", y);
+//        n=n/10;
+//    }
+//
+//}
+
+//int main(){
+//    int n;
+//    int r=0;
+//    
+//    printf("Enter the number: ");
+//    scanf("%d", &n);
+//    while (n>0){
+//        r=r*10;
+//        r = r+(n%10);
+//        
+//        
+//        n=n/10;
+//    
+//    }
+//    printf("%d", r);
+//
+//}
