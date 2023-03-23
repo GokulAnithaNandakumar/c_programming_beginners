93d97857c856f8a43892146f32c7ad243d7f2801
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-23 19:40:04 +0530

    Add files via upload

---
+++ patterns/simple.c
@@ -0,0 +1,36 @@
+//
+//  simple.c
+//  patterns
+//
+//  Created by Gokul Nandakumar on 20/02/23.
+//
+
+#include <stdio.h>
+//int main(){
+    /*
+     ***
+     ***
+     ***
+     */
+//    int n;
+//    printf("Enter a number: ");
+//    scanf("%d", &n);
+//    for (int i=1; i<=n; i++) {
+//        for (int i=1; i<=n; i++) {
+//            printf("*");}
+//        printf("\n");
+//    }
+    /*
+     1234
+     1234
+     1234
+     1234
+     */
+//    for (int i=1; i<=n; i++) {
+//        for (int i=1; i<=n; i++) {
+//            printf("%d", i);
+//        }
+//        printf("\n");
+//    }
+
+//}
