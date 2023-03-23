135c979cf2a4f0042d7b54da41ac034564425fe4
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-23 19:41:12 +0530

    Update simple.c

--- patterns/simple.c
+++ patterns/simple.c
@@ -1,36 +1,33 @@
 //
 //  simple.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 20/02/23.
-//
 
 #include <stdio.h>
-//int main(){
+int main(){
     /*
      ***
      ***
      ***
      */
-//    int n;
-//    printf("Enter a number: ");
-//    scanf("%d", &n);
-//    for (int i=1; i<=n; i++) {
-//        for (int i=1; i<=n; i++) {
-//            printf("*");}
-//        printf("\n");
-//    }
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (int i=1; i<=n; i++) {
+        for (int i=1; i<=n; i++) {
+            printf("*");}
+        printf("\n");
+    }
     /*
      1234
      1234
      1234
      1234
      */
-//    for (int i=1; i<=n; i++) {
-//        for (int i=1; i<=n; i++) {
-//            printf("%d", i);
-//        }
-//        printf("\n");
-//    }
+   for (int i=1; i<=n; i++) {
+       for (int i=1; i<=n; i++) {
+           printf("%d", i);
+       }
+       printf("\n");
+   }
 
-//}
+}
