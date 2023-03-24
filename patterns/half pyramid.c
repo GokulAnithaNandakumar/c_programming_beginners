601f2027ac1e963825645cc88ba41b30e1927d9e
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-24 12:07:19 +0530

    Add files via upload

---
+++ patterns/half pyramid.c
@@ -0,0 +1,50 @@
+//
+//  half pyramid.c
+//  patterns
+//
+//  Created by Gokul Nandakumar on 20/02/23.
+//
+
+#include <stdio.h>
+//int main(){
+//    /*
+//     *
+//     **
+//     ***
+//     */
+//    int n;
+//    printf("Enter a number: ");
+//    scanf("%d", &n);
+//    for (int i=1; i<=n; i++) {
+//        for (int k=1; k<=i; k++) {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+//
+//    /*
+//     1
+//     12
+//     123
+//     1234
+//     12345
+//     */
+//    for (int i=1; i<=n; i++) {
+//        for (int k=1; k<=i; k++) {
+//            printf("%d", k);
+//        }
+//        printf("\n");
+//    }
+//    /*
+//     1
+//     13
+//     135
+//     1357
+//     */
+//    for (int i=1; i<=n; i++) {
+//        for (int k=1; k<=2*i; k=k+2) {
+//            printf("%d", k);
+//        }
+//        printf("\n");
+//    }
+//}
