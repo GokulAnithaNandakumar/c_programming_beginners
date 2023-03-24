05f5ec99c8885abb146931bc07ac5db6aa366cd5
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-24 12:07:53 +0530

    Update half pyramid.c

--- patterns/half pyramid.c
+++ patterns/half pyramid.c
@@ -6,45 +6,45 @@
 //
 
 #include <stdio.h>
-//int main(){
-//    /*
-//     *
-//     **
-//     ***
-//     */
-//    int n;
-//    printf("Enter a number: ");
-//    scanf("%d", &n);
-//    for (int i=1; i<=n; i++) {
-//        for (int k=1; k<=i; k++) {
-//            printf("*");
-//        }
-//        printf("\n");
-//    }
-//
-//    /*
-//     1
-//     12
-//     123
-//     1234
-//     12345
-//     */
-//    for (int i=1; i<=n; i++) {
-//        for (int k=1; k<=i; k++) {
-//            printf("%d", k);
-//        }
-//        printf("\n");
-//    }
-//    /*
-//     1
-//     13
-//     135
-//     1357
-//     */
-//    for (int i=1; i<=n; i++) {
-//        for (int k=1; k<=2*i; k=k+2) {
-//            printf("%d", k);
-//        }
-//        printf("\n");
-//    }
-//}
+int main(){
+   /*
+    *
+    **
+    ***
+    */
+   int n;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   for (int i=1; i<=n; i++) {
+       for (int k=1; k<=i; k++) {
+           printf("*");
+       }
+       printf("\n");
+   }
+
+   /*
+    1
+    12
+    123
+    1234
+    12345
+    */
+   for (int i=1; i<=n; i++) {
+       for (int k=1; k<=i; k++) {
+           printf("%d", k);
+       }
+       printf("\n");
+   }
+   /*
+    1
+    13
+    135
+    1357
+    */
+   for (int i=1; i<=n; i++) {
+       for (int k=1; k<=2*i; k=k+2) {
+           printf("%d", k);
+       }
+       printf("\n");
+   }
+}
