713797a23e72d65d293c3346685b5471c38e18e8
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-25 09:58:54 +0530

    Add files via upload

---
+++ patterns/inverted half pyramid.c
@@ -0,0 +1,39 @@
+//
+//  inverted half pyramid.c
+//  patterns
+//
+//  Created by Gokul Nandakumar on 20/02/23.
+//
+
+#include <stdio.h>
+
+//int main(){
+    /*
+     ***
+     **
+     *
+     */
+//    int n;
+//    printf("Enter a number: ");
+//    scanf("%d", &n);
+//    for (int i=n; i>=1; i--) {
+//        for (int k=1; k<=i; k++) {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+    
+    /*
+     123
+     12
+     1
+     */
+
+//    for (int i=n; i>=1; i--) {
+//        for (int k=1; k<=i; k++) {
+//            printf("%d", k);
+//        }
+//        printf("\n");
+//    }
+//
+//}
