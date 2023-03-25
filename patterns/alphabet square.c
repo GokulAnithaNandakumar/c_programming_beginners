bdeed6d4b2b7c5224bb4d4b7e9f64c4e1166862b
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-26 04:58:27 +0530

    Update alphabet square.c

--- patterns/alphabet square.c
+++ patterns/alphabet square.c
@@ -1,22 +1,19 @@
 //
 //  alphabet square.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 20/02/23.
-//
 
-//#include <stdio.h>
-//int main(){
-//    int n;
-//    printf("Enter a number: ");
-//    scanf("%d", &n);
-//    for (int i=1; i<=n; i++) {
-//        
-//        
-//        for (int i = 65; i<=64+n; i++) {
-//            //typecasting
-//            //        char a = (char)i;
-//            printf("%c", i);
-//        }
-//        printf("\n");}
-//}
+#include <stdio.h>
+int main(){
+   int n;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   for (int i=1; i<=n; i++) {
+       
+       
+       for (int i = 65; i<=64+n; i++) {
+           //typecasting
+           //        char a = (char)i;
+           printf("%c", i);
+       }
+       printf("\n");}
+}
