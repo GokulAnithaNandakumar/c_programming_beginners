a3f2754f925380b456ba60adad537355452bd0ad
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-27 10:37:44 +0530

    Update 0 and 1 half triangle.c

--- patterns/0 and 1 half triangle.c
+++ patterns/0 and 1 half triangle.c
@@ -1,34 +1,31 @@
 //
 //  0 and 1 half triangle.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 20/02/23.
-//
 
-//#include <stdio.h>
-//int main(){
-//
-//    int n;
-//    printf("Enter a number: ");
-//    scanf("%d", &n);
-//    /*
-//     1
-//     2 3
-//     4 5 6
-//     7 8 9 10
-//     */
-//
-//    int a=1;
-//    int b=0;
-//
-//    for (int i=1; i<=n; i++) {
-//            for (int k=1; k<=i; k++) {
-//                if(i==k){
-//                    printf("%d ", a);}
-//                else if ((i+k)%2==0){printf("%d ", a);}
-//                else{printf("%d ", b);}
-//
-//        }
-//        printf("\n");
-//    }
-//}
+#include <stdio.h>
+int main(){
+
+   int n;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   /*
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+    */
+
+   int a=1;
+   int b=0;
+
+   for (int i=1; i<=n; i++) {
+           for (int k=1; k<=i; k++) {
+               if(i==k){
+                   printf("%d ", a);}
+               else if ((i+k)%2==0){printf("%d ", a);}
+               else{printf("%d ", b);}
+
+       }
+       printf("\n");
+   }
+}
