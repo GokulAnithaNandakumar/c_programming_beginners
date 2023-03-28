5dbc9a5ed291152df5a8b2dae631e13c46822d96
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-28 09:40:01 +0530

    Update alphabet half triangle.c

--- patterns/alphabet half triangle.c
+++ patterns/alphabet half triangle.c
@@ -1,22 +1,19 @@
 //
 //  alphabet half triangle.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 20/02/23.
-//
 
-//#include <stdio.h>
-//int main(){
-//    int n;
-//    printf("Enter a number: ");
-//    scanf("%d", &n);
-//    for (int i=1; i<=n; i++) {
-//        
-//        
-//        for (int k = 65; k<=64+i; k++) {
-//            //typecasting
-//            //        char a = (char)i;
-//            printf("%c", k);
-//        }
-//        printf("\n");}
-//}
+#include <stdio.h>
+int main(){
+   int n;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   for (int i=1; i<=n; i++) {
+       
+       
+       for (int k = 65; k<=64+i; k++) {
+           //typecasting
+           //        char a = (char)i;
+           printf("%c", k);
+       }
+       printf("\n");}
+}
