4aa55d9922639e3ec6809ccfd4dbd23011ff8f4c
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-28 09:39:34 +0530

    Add files via upload

---
+++ patterns/alphabet half triangle.c
@@ -0,0 +1,22 @@
+//
+//  alphabet half triangle.c
+//  patterns
+//
+//  Created by Gokul Nandakumar on 20/02/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    int n;
+//    printf("Enter a number: ");
+//    scanf("%d", &n);
+//    for (int i=1; i<=n; i++) {
+//        
+//        
+//        for (int k = 65; k<=64+i; k++) {
+//            //typecasting
+//            //        char a = (char)i;
+//            printf("%c", k);
+//        }
+//        printf("\n");}
+//}
