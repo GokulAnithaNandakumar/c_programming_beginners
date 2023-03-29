db5c6d4b76f9f7d49b8f9a73f75bb02dd219ab1e
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-29 18:08:44 +0530

    Update diamond.c

--- patterns/diamond.c
+++ patterns/diamond.c
@@ -1,32 +1,33 @@
 //
 //  diamond.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 20/02/23.
-//
 
-//#include <stdio.h>
-//int main(){
-//    int n;
-//    printf("Enter a number: ");
-//    scanf("%d", &n);
-//    int nsp =n- 1;
-//    for (int i=1; i<=n; i++) {
-//        int a=i-1;
-//        for (int k=1; k<=nsp; k++) {
-//            printf(" ");
-//        }nsp--;
-//        for (int j=1; j<=i; j++) {
-//            char ch=(char)(j+64);
-//            printf("%c",ch);
-//        }
-//        
-//        for (int k=1; k<=i-1; k++) {
-//            char ch=(char)(a+64);
-//            printf("%c", ch);
-//            a--;
-//        }
-////        nst = nst+2;
-//        printf("\n");
-//    }
-//}
+#include <stdio.h>
+int main(){
+  /*  A
+     ABA
+    ABCBA
+    */
+   int n;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   int nsp =n- 1;
+   for (int i=1; i<=n; i++) {
+       int a=i-1;
+       for (int k=1; k<=nsp; k++) {
+           printf(" ");
+       }nsp--;
+       for (int j=1; j<=i; j++) {
+           char ch=(char)(j+64);
+           printf("%c",ch);
+       }
+       
+       for (int k=1; k<=i-1; k++) {
+           char ch=(char)(a+64);
+           printf("%c", ch);
+           a--;
+       }
+//        nst = nst+2;
+       printf("\n");
+   }
+}
