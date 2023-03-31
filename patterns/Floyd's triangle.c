b12eeb688dcab4a77507acef7a03a72f307122bb
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-03-31 15:27:42 +0530

    Update Floyd's triangle.c

--- patterns/Floyd's triangle.c
+++ patterns/Floyd's triangle.c
@@ -1,30 +1,27 @@
 //
 //  Floyd's triangle.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 20/02/23.
-//
 
-//#include <stdio.h>
-//int main(){
-//
-//    int n;
-//    printf("Enter a number: ");
-//    scanf("%d", &n);
-//    /*
-//     1
-//     2 3
-//     4 5 6
-//     7 8 9 10
-//     */
-//
-//    int a=1;
-//
-//    for (int i=1; i<=n; i++) {
-//                for (int k=1; k<=i; k++) {
-//            printf("%d ", a);
-//            a++;
-//        }
-//        printf("\n");
-//    }
-//}
+#include <stdio.h>
+int main(){
+
+   int n;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   /*
+    1
+    2 3
+    4 5 6
+    7 8 9 10
+    */
+
+   int a=1;
+
+   for (int i=1; i<=n; i++) {
+               for (int k=1; k<=i; k++) {
+           printf("%d ", a);
+           a++;
+       }
+       printf("\n");
+   }
+}
