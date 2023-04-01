1a129208372ea32c8a8523a28357954362e9792d
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-01 10:12:23 +0530

    Add files via upload

---
+++ patterns/mixed pattern.c
@@ -0,0 +1,35 @@
+//
+//  mixed pattern.c
+//  patterns
+//
+//  Created by Gokul Nandakumar on 20/02/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    /*
+//     1
+//     AB
+//     123
+//     ABCD
+//     12345
+//     */
+//    int n;
+//    printf("Enter a number: ");
+//    scanf("%d", &n);
+//    for (int i=1; i<=n; i++) {
+//        if (i%2!=0){
+//            for (int k = 1; k<=i; k=k+1) {
+//                //typecasting
+//                //        char a = (char)i;
+//                printf("%d", k);
+//            }}
+//        else{
+//            for (int k = 65; k<=64+i; k=k+1) {
+//                //typecasting
+//                //        char a = (char)i;
+//                printf("%c", k);
+//            }}
+//
+//        printf("\n");}
+//}
