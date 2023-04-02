f04fe9baa31025eeeed96c7830350b96fb6fc3d4
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-02 06:52:45 +0530

    Update number pyramid new.c

--- patterns/number pyramid new.c
+++ patterns/number pyramid new.c
@@ -1,29 +1,26 @@
 //
 //  number pyramid new.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 20/02/23.
-//
 
-//#include <stdio.h>
-//int main(){
-//    int n;
-//    printf("Enter a number: ");
-//    scanf("%d", &n);
-//    int nsp =n- 1;
-//    for (int i=1; i<=n; i++) {
-//        int a=i-1;
-//        for (int k=1; k<=nsp; k++) {
-//            printf(" ");
-//        }nsp--;
-//        for (int j=1; j<=i; j++) {
-//            printf("%d",j);
-//        }
-//        for (int k=1; k<=i-1; k++) {
-//            printf("%d", a);
-//            a--;
-//        }
-////        nst = nst+2;
-//        printf("\n");
-//    }
-//}
+#include <stdio.h>
+int main(){
+   int n;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   int nsp =n- 1;
+   for (int i=1; i<=n; i++) {
+       int a=i-1;
+       for (int k=1; k<=nsp; k++) {
+           printf(" ");
+       }nsp--;
+       for (int j=1; j<=i; j++) {
+           printf("%d",j);
+       }
+       for (int k=1; k<=i-1; k++) {
+           printf("%d", a);
+           a--;
+       }
+//        nst = nst+2;
+       printf("\n");
+   }
+}
