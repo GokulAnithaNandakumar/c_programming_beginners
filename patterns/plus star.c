2bab25b7e9e4d1ffe4bfb1bd8829f6ef25c887f9
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-05 11:23:41 +0530

    Add files via upload

---
+++ patterns/plus star.c
@@ -0,0 +1,23 @@
+//
+//  plus star.c
+//  patterns
+//
+//  Created by Gokul Nandakumar on 20/02/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    int n;
+//    printf("Enter a number: ");
+//    scanf("%d", &n);
+//    
+//    for(int i=1; i<=n; i++) {
+//        for (int j=1; j<=n; j++) {
+//            if(j==(n/2)+1||i==(n/2)+1){
+//                printf("*");
+//            }
+//            else{printf(" ");}
+//        }
+//        printf("\n");
+//    }
+//}
