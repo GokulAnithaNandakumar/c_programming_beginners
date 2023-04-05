7988081e7d2029ce569c9b367ab1eed3fca46a5d
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-05 11:24:04 +0530

    Update plus star.c

--- patterns/plus star.c
+++ patterns/plus star.c
@@ -1,23 +1,20 @@
 //
 //  plus star.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 20/02/23.
-//
 
-//#include <stdio.h>
-//int main(){
-//    int n;
-//    printf("Enter a number: ");
-//    scanf("%d", &n);
-//    
-//    for(int i=1; i<=n; i++) {
-//        for (int j=1; j<=n; j++) {
-//            if(j==(n/2)+1||i==(n/2)+1){
-//                printf("*");
-//            }
-//            else{printf(" ");}
-//        }
-//        printf("\n");
-//    }
-//}
+#include <stdio.h>
+int main(){
+   int n;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   
+   for(int i=1; i<=n; i++) {
+       for (int j=1; j<=n; j++) {
+           if(j==(n/2)+1||i==(n/2)+1){
+               printf("*");
+           }
+           else{printf(" ");}
+       }
+       printf("\n");
+   }
+}
