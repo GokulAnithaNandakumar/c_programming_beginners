457c9cbc03bd30f3a1706c4cd9ffb2370469132c
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-06 11:23:22 +0530

    Update rectangle star.c

--- patterns/rectangle star.c
+++ patterns/rectangle star.c
@@ -1,45 +1,42 @@
 //
 //  rectangle star.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 20/02/23.
-//
 
-//#include<stdio.h>
-//int main(){
-//    /*
-//     ******
-//     *    *
-//     *    *
-//     ******
-//     */
-//    int n, m;
-//    printf("Enter the number of rows: ");
-//    scanf("%d", &n);
-//    printf("Enter the number of coulmns: ");
-//    scanf("%d", &m);
-//    for (int i=1; i<=n; i++) {
-//        if(i==1){
-//            for (int k=1; k<=m; k++) {
-//                printf("*");
-//            }
-////            printf("\n");
-//        }else if(i==n){
-//            for (int k=1; k<=m; k++) {
-//                printf("*");}
+#include<stdio.h>
+int main(){
+   /*
+    ******
+    *    *
+    *    *
+    ******
+    */
+   int n, m;
+   printf("Enter the number of rows: ");
+   scanf("%d", &n);
+   printf("Enter the number of coulmns: ");
+   scanf("%d", &m);
+   for (int i=1; i<=n; i++) {
+       if(i==1){
+           for (int k=1; k<=m; k++) {
+               printf("*");
+           }
 //            printf("\n");
-//        }
-//        else{
-//            for (int j=1; j<=m; j++) {
-//                if (j==1||j==m) {
-//                    printf("*");
-//                }
-//
-//                else{
-//                    printf(" ");
-//                }
-//            }
-//        }printf("\n");
-//
-//    }
-//}
+       }else if(i==n){
+           for (int k=1; k<=m; k++) {
+               printf("*");}
+           printf("\n");
+       }
+       else{
+           for (int j=1; j<=m; j++) {
+               if (j==1||j==m) {
+                   printf("*");
+               }
+
+               else{
+                   printf(" ");
+               }
+           }
+       }printf("\n");
+
+   }
+}
