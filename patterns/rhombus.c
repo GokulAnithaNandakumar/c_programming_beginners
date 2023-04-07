f327dceb58728a898468c7271801fca1dd564b35
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-07 09:50:59 +0530

    Update rhombus.c

--- patterns/rhombus.c
+++ patterns/rhombus.c
@@ -1,24 +1,21 @@
 //
 //  rhombus.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 20/02/23.
-//
-//
-//#include <stdio.h>
-//int main(){
-//    int n;
-//    printf("Enter the side value: ");
-//    scanf("%d", &n);
-//    for (int i=n; i>=1; i--) {
-//        for (int k=1; k<=i; k++) {
-//            printf(" ");
-//        }
-//        for (int i=1; i<=n; i++) {
-//            printf("*");
-//        }
-//        
-//        
-//        printf("\n");
-//        
-//    }}
+
+#include <stdio.h>
+int main(){
+   int n;
+   printf("Enter the side value: ");
+   scanf("%d", &n);
+   for (int i=n; i>=1; i--) {
+       for (int k=1; k<=i; k++) {
+           printf(" ");
+       }
+       for (int i=1; i<=n; i++) {
+           printf("*");
+       }
+       
+       
+       printf("\n");
+       
+   }}
