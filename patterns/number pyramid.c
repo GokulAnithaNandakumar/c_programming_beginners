522c11c9188af954da68a23ea8598b83da8e9e47
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-09 08:36:01 +0530

    Add files via upload

---
+++ patterns/number pyramid.c
@@ -0,0 +1,24 @@
+//
+//  number pyramid.c
+//  patterns
+//
+//  Created by Gokul Nandakumar on 20/02/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    int n;
+//    printf("Enter a number: ");
+//    scanf("%d", &n);
+//    int nst = 1;
+//    for (int i=1; i<=n; i++) {
+//        for (int k=1; k<=n-i; k++) {
+//            printf(" ");
+//        }
+//        for (int j=1; j<=nst; j++) {
+//            printf("%d",j);
+//        }
+//        nst = nst+2;
+//        printf("\n");
+//    }
+//}
