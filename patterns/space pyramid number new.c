051ee502914d8aa8b9ee0ed70f5543a1e122b708
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-10 07:41:46 +0530

    Update space pyramid number new.c

--- patterns/space pyramid number new.c
+++ patterns/space pyramid number new.c
@@ -1,42 +1,39 @@
 //
 //  space pyramid number new.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 21/02/23.
-//
 
-//#include <stdio.h>
-//
-//int main(){
-//    int n;
-//    printf("Enter the side value: ");
-//    scanf("%d", &n);
-//    for (int i=1; i<=(n*2)-1; i++) {
-//        printf("%d", i);
-//
-//    }
-//    printf("\n");
-//    for (int i=n-1; i>=1; i--) {
-//        
-//        for (int k=1; k<=i; k++) {
-//            printf("%d", k);
-//           
-//        }
-//        for (int l=i; l<=n-1; l++) {
-//            printf(" ");
-//            
-//        }
-//        for (int d=i; d<=n-2; d++) {
-//            printf(" ");
-//            
-//        }
-//
-//        for (int j=i; j>=1; j--) {
-//            printf("%d", j);
-//            
-//        }
-//
-//
-//        printf("\n");
-//
-//    }}
+#include <stdio.h>
+
+int main(){
+   int n;
+   printf("Enter the side value: ");
+   scanf("%d", &n);
+   for (int i=1; i<=(n*2)-1; i++) {
+       printf("%d", i);
+
+   }
+   printf("\n");
+   for (int i=n-1; i>=1; i--) {
+       
+       for (int k=1; k<=i; k++) {
+           printf("%d", k);
+          
+       }
+       for (int l=i; l<=n-1; l++) {
+           printf(" ");
+           
+       }
+       for (int d=i; d<=n-2; d++) {
+           printf(" ");
+           
+       }
+
+       for (int j=i; j>=1; j--) {
+           printf("%d", j);
+           
+       }
+
+
+       printf("\n");
+
+   }}
