9f76fb21d373e126bb5ff4624da0375c881bd378
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-11 12:15:38 +0530

    Add files via upload

---
+++ patterns/space pyramid.c
@@ -0,0 +1,65 @@
+//
+//  space pyramid.c
+//  patterns
+//
+//  Created by Gokul Nandakumar on 21/02/23.
+//
+//My code!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+#include <stdio.h>
+//int main(){
+//    int n;
+//    printf("Enter the side value: ");
+//    scanf("%d", &n);
+//    for (int i=1; i<=(n*2)-1; i++) {
+//        printf("*");
+//    
+//    }
+//    printf("\n");
+//    for (int i=n-1; i>=1; i--) {
+//        for (int k=1; k<=i; k++) {
+//            printf("*");
+//        }
+//        for (int l=i; l<=n-1; l++) {
+//            printf(" ");
+//        }
+//        for (int d=i; d<=n-2; d++) {
+//            printf(" ");
+//        }
+//
+//        for (int j=i; j>=1; j--) {
+//            printf("*");
+//        }
+//
+//
+//        printf("\n");
+//
+//    }}
+//Sir code
+//int main(){
+//    int n;
+//    printf("Enter the side value: ");
+//    scanf("%d", &n);
+//    int nst=n;
+//    int nsp=1;
+//    for (int i=1; i<=(n*2)+1; i++) {
+//        printf("*");
+//
+//    }
+//    printf("\n");
+//    for (int i=1; i<=n; i++) {
+//        for (int k=1; k<=nst; k++) {
+//            printf("*");
+//        }
+//
+//        for (int d=1; d<=nsp; d++) {
+//            printf(" ");
+//        }
+//
+//        for (int j=1; j<=nst; j++) {
+//            printf("*");
+//        }
+//        nst--;
+//        nsp+=2;
+//        printf("\n");
+//
+//    }}
