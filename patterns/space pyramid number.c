511ffa5b35e662f5940c74f0eacb9cf570c17ad6
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-11 12:48:47 +0530

    Update space pyramid number.c

--- patterns/space pyramid number.c
+++ patterns/space pyramid number.c
@@ -1,76 +1,73 @@
 //
 //  space pyramid number.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 21/02/23.
-//
 
 #include <stdio.h>
-//my code
-//int main(){
-//    int n;
-//    printf("Enter the side value: ");
-//    scanf("%d", &n);
-//    for (int i=1; i<=(n*2)-1; i++) {
-//        printf("%d", i);
-//
-//    }
-//    printf("\n");
-//    for (int i=n-1; i>=1; i--) {
-//        int a=1;
-//        for (int k=1; k<=i; k++) {
-//            printf("%d", a);
-//            a++;
-//        }
-//        for (int l=i; l<=n-1; l++) {
-//            printf(" ");
-//            a++;
-//        }
-//        for (int d=i; d<=n-2; d++) {
-//            printf(" ");
-//            a++;
-//        }
-//
-//        for (int j=i; j>=1; j--) {
-//            printf("%d", a);
-//            a++;
-//        }
-//
-//
-//        printf("\n");
-//
-//    }}
 
-//sir code
-//int main(){
-//    int n;
-//    printf("Enter the side value: ");
-//    scanf("%d", &n);
-//    int nst=n;
-//    int nsp=1;
-//    for (int i=1; i<=(n*2)+1; i++) {
-//        printf("%d", i);
-//
-//    }
-//    printf("\n");
-//    for (int i=1; i<=n; i++) {
-//        int a=1;
-//        for (int k=1; k<=nst; k++) {
-//            printf("%d", a);
-//            a++;
-//        }
-//
-//        for (int d=1; d<=nsp; d++) {
-//            printf(" ");
-//            a++;
-//        }
-//
-//        for (int j=1; j<=nst; j++) {
-//            printf("%d", a);
-//            a++;
-//        }
-//        nst--;
-//        nsp+=2;
-//        printf("\n");
-//
-//    }}
+int main(){
+   int n;
+   printf("Enter the side value: ");
+   scanf("%d", &n);
+   for (int i=1; i<=(n*2)-1; i++) {
+       printf("%d", i);
+
+   }
+   printf("\n");
+   for (int i=n-1; i>=1; i--) {
+       int a=1;
+       for (int k=1; k<=i; k++) {
+           printf("%d", a);
+           a++;
+       }
+       for (int l=i; l<=n-1; l++) {
+           printf(" ");
+           a++;
+       }
+       for (int d=i; d<=n-2; d++) {
+           printf(" ");
+           a++;
+       }
+
+       for (int j=i; j>=1; j--) {
+           printf("%d", a);
+           a++;
+       }
+
+
+       printf("\n");
+
+   }}
+
+//Other method
+int main(){
+   int n;
+   printf("Enter the side value: ");
+   scanf("%d", &n);
+   int nst=n;
+   int nsp=1;
+   for (int i=1; i<=(n*2)+1; i++) {
+       printf("%d", i);
+
+   }
+   printf("\n");
+   for (int i=1; i<=n; i++) {
+       int a=1;
+       for (int k=1; k<=nst; k++) {
+           printf("%d", a);
+           a++;
+       }
+
+       for (int d=1; d<=nsp; d++) {
+           printf(" ");
+           a++;
+       }
+
+       for (int j=1; j<=nst; j++) {
+           printf("%d", a);
+           a++;
+       }
+       nst--;
+       nsp+=2;
+       printf("\n");
+
+   }}
