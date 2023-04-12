590ec1fe76df2b9b3cc1b2e3cd20e3188ee78b6f
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-12 08:28:45 +0530

    Update star diamond.c

--- patterns/star diamond.c
+++ patterns/star diamond.c
@@ -1,36 +1,34 @@
 //
 //  star diamonf.c
 //  patterns
-//
-//  Created by Gokul Nandakumar on 21/02/23.
-//
-//#include <stdio.h>
-//int main(){
-//    int n;
-//    printf("Enter a number: ");
-//    scanf("%d", &n);
-//    int nsp =n/2;//number of spaces   =    ml-1 == n/2
-//    int nst = 1; // number of stars
-//    int ml= n/2 + 1; //middle line
-//    for (int i=1; i<=n; i++) {
-//        for (int k=1; k<=nsp; k++) {
-//            printf(" ");
-//        }
-//        for (int j=1; j<=nst; j++) {
-//            
-//            printf("*");
-//        }
-//        
-//        if(i<ml){
-//            nsp--;
-//            nst+=2;
-//        }
-//        else{
-//            nsp++;
-//            nst-=2;
-//        }
-//        
-//        printf("\n");
-//    }
-//    
-//}
+
+#include <stdio.h>
+int main(){
+   int n;
+   printf("Enter a number: ");
+   scanf("%d", &n);
+   int nsp =n/2;//number of spaces   =    ml-1 == n/2
+   int nst = 1; // number of stars
+   int ml= n/2 + 1; //middle line
+   for (int i=1; i<=n; i++) {
+       for (int k=1; k<=nsp; k++) {
+           printf(" ");
+       }
+       for (int j=1; j<=nst; j++) {
+           
+           printf("*");
+       }
+       
+       if(i<ml){
+           nsp--;
+           nst+=2;
+       }
+       else{
+           nsp++;
+           nst-=2;
+       }
+       
+       printf("\n");
+   }
+   
+}
