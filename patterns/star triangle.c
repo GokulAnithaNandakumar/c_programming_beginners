6ea956f7b212e42b7e9860ded53ad615fb6f0544
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-13 11:36:19 +0530

    Add files via upload

---
+++ patterns/star triangle.c
@@ -0,0 +1,22 @@
+//
+//  star triangle.c
+//  patterns
+//
+//  Created by Gokul Nandakumar on 20/02/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    int n;
+//    printf("Enter a number: ");
+//    scanf("%d", &n);
+//    for (int i=1; i<=n; i++) {
+//        for (int k=1; k<=n-i; k++) {
+//            printf(" ");
+//        }
+//        for (int j=1; j<=i; j++) {
+//            printf("*");
+//        }
+//        printf("\n");
+//    }
+//}
