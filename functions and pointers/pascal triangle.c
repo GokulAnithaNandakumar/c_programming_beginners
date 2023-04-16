f654f5ba1f4e1bc3161e66f5b39f6a76f0de257d
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-16 10:31:38 +0530

    Update pascal triangle.c

--- functions and pointers/pascal triangle.c
+++ functions and pointers/pascal triangle.c
@@ -1,32 +1,29 @@
 //
 //  pascal triangle.c
 //  functions and pointers
-//
-//  Created by Gokul Nandakumar on 23/02/23.
-//
 
 #include <stdio.h>
 
-//int fact(int n){
-//    int a=1;
-//    for (int i=2; i<=n; i++) {
-//        a=a*i;
-//
-//    }return a;
-//}
-//
-//int combnation(int a, int b){
-//    int comb=fact(a)/(fact(b)*fact(a-b));
-//    return comb;
-//}
-//int main(){
-//    int n;
-//    printf("Enter the number of lines: ");
-//    scanf("%d", &n);
-//    for (int i=0; i<n; i++) {
-//        for (int j=0; j<=i; j++) {
-//            int iCj=combnation(i, j);
-//            printf("%d ", iCj);
-//        }printf("\n");
-//    }
-//}
+int fact(int n){
+   int a=1;
+   for (int i=2; i<=n; i++) {
+       a=a*i;
+
+   }return a;
+}
+
+int combnation(int a, int b){
+   int comb=fact(a)/(fact(b)*fact(a-b));
+   return comb;
+}
+int main(){
+   int n;
+   printf("Enter the number of lines: ");
+   scanf("%d", &n);
+   for (int i=0; i<n; i++) {
+       for (int j=0; j<=i; j++) {
+           int iCj=combnation(i, j);
+           printf("%d ", iCj);
+       }printf("\n");
+   }
+}
