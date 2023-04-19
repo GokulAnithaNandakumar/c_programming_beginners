b4cfecf72f2fe2255786354825be665f5165bd2f
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-19 15:55:29 +0530

    Add files via upload

---
+++ functions and pointers/swap two number without third variable ***.c
@@ -0,0 +1,31 @@
+//
+//  swap two number without third variable ***.c
+//  functions and pointers
+//
+//  Created by Gokul Nandakumar on 23/02/23.
+//
+
+//#include <stdio.h>
+//void swap(int a, int b){
+//    int sum =a+b;
+//    a=sum-a;
+//    b=sum-b;
+//    printf("The value of first number is: %d\n",a);
+//    printf("The value of second number is: %d\n",b);
+//    return ;
+//}
+//int main(){
+//    int x,y;
+//    printf("Enter a number: ");
+//    scanf("%d", &x);
+//    printf("Enter a number: ");
+//    scanf("%d", &y);
+////    int sum =x+y;
+////    x=sum-x;
+////    y=sum-y;
+//    swap(x,y);
+////    printf("The value of first number is: %d\n",x);
+////    printf("The value of second number is: %d\n",y);
+//}
+  
+
