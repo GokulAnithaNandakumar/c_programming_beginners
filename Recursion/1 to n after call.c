5172a2359c805d9b8aa5613701820883e6aaaabe
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-20 11:09:09 +0530

    Update 1 to n after call.c

--- Recursion/1 to n after call.c
+++ Recursion/1 to n after call.c
@@ -2,16 +2,16 @@
 //  1 to n after call.c
 //  Recursion
 //
-//  Created by Gokul Nandakumar on 28/02/23.
+//  Created by Gokul Nandakumar
 
-//#include <stdio.h>
-//void increasing(int n){
-//
-//    if(n==0) return;
-//    increasing(n-1);
-//    printf("%d", n);
-//    return;
-//}
-//int main(){
-//    increasing(5);
-//}
+#include <stdio.h>
+void increasing(int n){
+
+   if(n==0) return;
+   increasing(n-1);
+   printf("%d", n);
+   return;
+}
+int main(){
+   increasing(5);
+}
