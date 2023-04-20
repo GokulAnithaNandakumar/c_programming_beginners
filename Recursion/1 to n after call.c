5d492a36bf6503287b05f795e4c4bf04aa458072
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-20 11:08:34 +0530

    Add files via upload

---
+++ Recursion/1 to n after call.c
@@ -0,0 +1,17 @@
+//
+//  1 to n after call.c
+//  Recursion
+//
+//  Created by Gokul Nandakumar on 28/02/23.
+
+//#include <stdio.h>
+//void increasing(int n){
+//
+//    if(n==0) return;
+//    increasing(n-1);
+//    printf("%d", n);
+//    return;
+//}
+//int main(){
+//    increasing(5);
+//}
