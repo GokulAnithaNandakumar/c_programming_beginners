a9805399df45641561687d1199e5db053c3897d5
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-21 09:55:12 +0530

    Add files via upload

---
+++ Recursion/exponent.c
@@ -0,0 +1,19 @@
+//
+//  exponent.c
+//  Recursion
+//
+//  Created by Gokul Nandakumar on 28/02/23.
+//
+
+//#include<stdio.h>
+//int power(int a, int b){
+//    if(b==0) return 1;
+//    else {return a* power(a, b-1);}
+//        
+//        
+//    
+//}
+//int main(){
+//    int a = power(5, 3);
+//    printf("%d", a);
+//}
