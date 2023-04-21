07569b7b5899eaec9701ac15444dadbf3610b9e7
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-21 09:55:39 +0530

    Update exponent.c

--- Recursion/exponent.c
+++ Recursion/exponent.c
@@ -2,18 +2,17 @@
 //  exponent.c
 //  Recursion
 //
-//  Created by Gokul Nandakumar on 28/02/23.
-//
+//  Created by Gokul Nandakumar
 
-//#include<stdio.h>
-//int power(int a, int b){
-//    if(b==0) return 1;
-//    else {return a* power(a, b-1);}
-//        
-//        
-//    
-//}
-//int main(){
-//    int a = power(5, 3);
-//    printf("%d", a);
-//}
+#include<stdio.h>
+int power(int a, int b){
+   if(b==0) return 1;
+   else {return a* power(a, b-1);}
+       
+       
+   
+}
+int main(){
+   int a = power(5, 3);
+   printf("%d", a);
+}
