8a4706ef14b23ca8c3215e88cdf5ae7834e283b9
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-22 22:44:41 +0530

    Add files via upload

---
+++ Recursion/factorial recursion.c
@@ -0,0 +1,21 @@
+//
+//  factorial recursion.c
+//  Recursion
+//
+//  Created by Gokul Nandakumar on 26/02/23.
+//
+
+#include <stdio.h>
+//int fac(int n){
+//    if(n==1||n==0) return 1;   
+//    else {return n * fac(n-1);}
+//
+//}
+//int main(){
+//    int b;
+//    printf("Enter a number: ");
+//    scanf("%d", &b);
+//    int a = fac(b);
+//    printf("%d", a);
+//}
+ 
