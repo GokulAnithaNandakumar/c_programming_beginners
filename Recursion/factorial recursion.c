efc0629de8c890d2c6b3ad0683769fba2eee3b84
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-22 22:45:03 +0530

    Update factorial recursion.c

--- Recursion/factorial recursion.c
+++ Recursion/factorial recursion.c
@@ -2,20 +2,19 @@
 //  factorial recursion.c
 //  Recursion
 //
-//  Created by Gokul Nandakumar on 26/02/23.
-//
+//  Created by Gokul Nandakumar
 
 #include <stdio.h>
-//int fac(int n){
-//    if(n==1||n==0) return 1;   
-//    else {return n * fac(n-1);}
-//
-//}
-//int main(){
-//    int b;
-//    printf("Enter a number: ");
-//    scanf("%d", &b);
-//    int a = fac(b);
-//    printf("%d", a);
-//}
+int fac(int n){
+   if(n==1||n==0) return 1;   
+   else {return n * fac(n-1);}
+
+}
+int main(){
+   int b;
+   printf("Enter a number: ");
+   scanf("%d", &b);
+   int a = fac(b);
+   printf("%d", a);
+}
  
