dad33947c6f2d085391a470def4c9bbf04f91f2b
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-25 14:26:25 +0530

    Update power function logarithmic.c

--- Recursion/power function logarithmic.c
+++ Recursion/power function logarithmic.c
@@ -2,17 +2,16 @@
 //  power function logarithmic.c
 //  Recursion
 //
-//  Created by Gokul Nandakumar on 28/02/23.
-//
+//  Created by Gokul Nandakumar
 
 #include <stdio.h>
 
-//int powerlog(int a, int b){
-//    if(b==0) return 1;
-//    int x = powerlog(a, b/2);
-//    if(b%2==0) return x*x;
-//    else return x*x*a;
-//}
-//int main(){
-//    printf("%d", powerlog(2, 3));
-//}
+int powerlog(int a, int b){
+   if(b==0) return 1;
+   int x = powerlog(a, b/2);
+   if(b%2==0) return x*x;
+   else return x*x*a;
+}
+int main(){
+   printf("%d", powerlog(2, 3));
+}
