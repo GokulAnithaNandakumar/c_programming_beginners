78f722029bb3981de1403cf22aa50c2563e6fbf8
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-25 14:25:59 +0530

    Add files via upload

---
+++ Recursion/power function logarithmic.c
@@ -0,0 +1,18 @@
+//
+//  power function logarithmic.c
+//  Recursion
+//
+//  Created by Gokul Nandakumar on 28/02/23.
+//
+
+#include <stdio.h>
+
+//int powerlog(int a, int b){
+//    if(b==0) return 1;
+//    int x = powerlog(a, b/2);
+//    if(b%2==0) return x*x;
+//    else return x*x*a;
+//}
+//int main(){
+//    printf("%d", powerlog(2, 3));
+//}
