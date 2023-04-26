3728237466a52f70b77b2a7b0d3a63f583d96162
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-26 11:42:04 +0530

    Add files via upload

---
+++ arrays/2d sum.c
@@ -0,0 +1,22 @@
+//
+//  2d sum.c
+//  Arrays
+//
+//  Created by Gokul Nandakumar on 04/03/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    int ar1[2][3] = {1,2,3,4,5,6}; int ar2[2][3] = {1,2,3,4,5,6}; int ar3[2][3];
+//    for (int i=0; i<2; i++) {
+//        for (int j=0; j<3; j++) {
+//            int n=ar1[i][j]+ar2[i][j];
+//            ar3[i][j]=n;
+//        }
+//    }
+//    for (int i=0; i<2; i++) {
+//        for (int j=0; j<3; j++) {
+//            printf("%d\t", ar3[i][j]);}
+//    }
+//    
+//}
