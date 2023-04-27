5ba1fb1b8aecbf462667daaa980c30f17c668e55
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-27 11:02:03 +0530

    Add files via upload

---
+++ arrays/duplicate element.c
@@ -0,0 +1,16 @@
+//
+//  duplicate element.c
+//  Arrays
+//
+//  Created by Gokul Nandakumar on 09/03/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    int a[]={1,2,3,7,5,6,7};
+//    for (int i=0; i<7; i++) {
+//        for (int j=i+1; j<7; j++) {
+//            if(a[i]==a[j]) {printf("%d is a duplicate.", a[i]); break;}
+//        }
+//    }
+//}
