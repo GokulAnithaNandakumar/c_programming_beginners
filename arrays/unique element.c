1864b1bc8046370f5962a802635995f17747cbfc
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-04-30 19:29:39 +0530

    Update unique element.c

--- arrays/unique element.c
+++ arrays/unique element.c
@@ -2,20 +2,19 @@
 //  unique element.c
 //  Arrays
 //
-//  Created by Gokul Nandakumar on 09/03/23.
-//
+//  Created by Gokul Nandakumar
 
-//#include <stdio.h>
-//#include <stdbool.h>
-//int main(){
-//    int a[]={1,3,6,1,1,2,3};
-//    for (int i=0; i<7; i++) {
-//        bool flag=false;
-//        for (int j=i+1; j<7; j++) {
-//            if(a[i]==a[j]){flag=true;}
-//        }
-//        if(flag==false) {printf("%d", a[i]);break;}
-//    }
-//    
-//    
-//}
+#include <stdio.h>
+#include <stdbool.h>
+int main(){
+   int a[]={1,3,6,1,1,2,3};
+   for (int i=0; i<7; i++) {
+       bool flag=false;
+       for (int j=i+1; j<7; j++) {
+           if(a[i]==a[j]){flag=true;}
+       }
+       if(flag==false) {printf("%d", a[i]);break;}
+   }
+   
+   
+}
