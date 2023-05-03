5d645368a2465f5f4d9f7b88acc2fda9af1c67f5
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-05-03 23:28:45 +0530

    Update reverse an array without extra array.c

--- arrays/reverse an array without extra array.c
+++ arrays/reverse an array without extra array.c
@@ -2,21 +2,20 @@
 //  reverse an array without extra array.c
 //  Arrays
 //
-//  Created by Gokul Nandakumar on 07/03/23.
-//
+//  Created by Gokul Nandakumar
 
-//#include <stdio.h>
-//void reverse(int a[]){
-//    for (int i=0,j=6; i<j; i++,j--) {
-//        int temp=a[i];
-//        a[i]=a[j];
-//        a[j]=temp;
-//    }return;
-//}
-//int main(){
-//    int a[]={1,2,3,4,5,6,7};
-//    reverse(a);
-//    for (int i=0; i<7; i++) {
-//        printf("%d ", a[i]);
-//    }
-//}
+#include <stdio.h>
+void reverse(int a[]){
+   for (int i=0,j=6; i<j; i++,j--) {
+       int temp=a[i];
+       a[i]=a[j];
+       a[j]=temp;
+   }return;
+}
+int main(){
+   int a[]={1,2,3,4,5,6,7};
+   reverse(a);
+   for (int i=0; i<7; i++) {
+       printf("%d ", a[i]);
+   }
+}
