4b16b1a6bfd6c1604ca954f99d22f0aff81debcc
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-05-04 17:00:17 +0530

    Update passing array to function.c

--- arrays/passing array to function.c
+++ arrays/passing array to function.c
@@ -2,31 +2,30 @@
 //  passing array to function.c
 //  Arrays
 //
-//  Created by Gokul Nandakumar on 07/03/23.
-//
+//  Created by Gokul Nandakumar
 
 #include <stdio.h>
-//void fun(int a[]){
-//    a[0]=10;
-//}
-//
-//int main(){
-//    int arr[10]={1,2,3,4,5};
-//    printf("%d\n", arr[0]);
-//    fun(arr);
-//    printf("%d", arr[0]);
-//}
+void fun(int a[]){
+   a[0]=10;
+}
 
-//void fun(int a[]){
-//    int temp=a[0];
-//    a[0]=a[1];
-//    a[1]=temp;
-//}
-//
-//int main(){
-//    int arr[10]={1,2};
-//    printf("%d %d\n", arr[0],arr[1]);
-//    fun(arr);
-//    printf("%d %d\n", arr[0],arr[1]);
-//}
-//
+int main(){
+   int arr[10]={1,2,3,4,5};
+   printf("%d\n", arr[0]);
+   fun(arr);
+   printf("%d", arr[0]);
+}
+
+void fun(int a[]){
+   int temp=a[0];
+   a[0]=a[1];
+   a[1]=temp;
+}
+
+int main(){
+   int arr[10]={1,2};
+   printf("%d %d\n", arr[0],arr[1]);
+   fun(arr);
+   printf("%d %d\n", arr[0],arr[1]);
+}
+
