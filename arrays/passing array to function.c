ab8b5e7f66f4c047063443d543d11ec94fa4d5c2
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-05-04 16:59:56 +0530

    Add files via upload

---
+++ arrays/passing array to function.c
@@ -0,0 +1,32 @@
+//
+//  passing array to function.c
+//  Arrays
+//
+//  Created by Gokul Nandakumar on 07/03/23.
+//
+
+#include <stdio.h>
+//void fun(int a[]){
+//    a[0]=10;
+//}
+//
+//int main(){
+//    int arr[10]={1,2,3,4,5};
+//    printf("%d\n", arr[0]);
+//    fun(arr);
+//    printf("%d", arr[0]);
+//}
+
+//void fun(int a[]){
+//    int temp=a[0];
+//    a[0]=a[1];
+//    a[1]=temp;
+//}
+//
+//int main(){
+//    int arr[10]={1,2};
+//    printf("%d %d\n", arr[0],arr[1]);
+//    fun(arr);
+//    printf("%d %d\n", arr[0],arr[1]);
+//}
+//
