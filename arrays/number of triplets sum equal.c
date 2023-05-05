0b3d8f69a4a519fbaf2f889577ebdfa54ebec114
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-05-05 13:34:32 +0530

    Update number of triplets sum equal.c

--- arrays/number of triplets sum equal.c
+++ arrays/number of triplets sum equal.c
@@ -2,22 +2,21 @@
 //  number of triplets sum equal.c
 //  Arrays
 //
-//  Created by Gokul Nandakumar on 07/03/23.
-//
+//  Created by Gokul Nandakumar
 
 #include <stdio.h>
-//int main(){
-//    int arr[]={1,2,3,4,5,6,7,8,9,10};
-//    int a=12;
-//    int count = 0;
-//    for (int i=0; i<10; i++) {
-//        for (int j=i+1; j<10; j++) {
-//            for (int k=j+1; k<10; k++) {
-//                if(arr[i]+arr[j]+arr[k]==a) {count+=1; printf("(%d,%d,%d)\n", arr[i],arr[j],arr[k]);}
-//                
-//                
-//                
-//            }
-//        }
-//    }printf("%d", count);
-//}
+int main(){
+   int arr[]={1,2,3,4,5,6,7,8,9,10};
+   int a=12;
+   int count = 0;
+   for (int i=0; i<10; i++) {
+       for (int j=i+1; j<10; j++) {
+           for (int k=j+1; k<10; k++) {
+               if(arr[i]+arr[j]+arr[k]==a) {count+=1; printf("(%d,%d,%d)\n", arr[i],arr[j],arr[k]);}
+               
+               
+               
+           }
+       }
+   }printf("%d", count);
+}
