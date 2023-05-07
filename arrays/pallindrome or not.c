4a5c23e823b2422f1d48795e66d74308d61f2e84
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-05-07 20:43:02 +0530

    Add files via upload

---
+++ arrays/pallindrome or not.c
@@ -0,0 +1,19 @@
+//
+//  pallindrome or not.c
+//  Arrays
+//
+//  Created by Gokul Nandakumar on 07/03/23.
+//
+
+//#include <stdio.h>
+//int main(){
+//    int a[]={1,2,3,4,5,5,4,3,2,1};
+//    int counter=0;
+//    for (int i=0, j=9; i<10, j>=0; i++, j--){
+//        if(a[i]==a[j]) continue;
+//        else {counter=1; break;}
+//    }
+//    if(counter==1) printf("No");
+//    else printf("Yes");
+//}
+//
