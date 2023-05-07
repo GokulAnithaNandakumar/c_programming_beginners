cc13ffbab3ec43f541ca0a4c74be7b88a9fe86c8
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-05-07 20:43:26 +0530

    Update pallindrome or not.c

--- arrays/pallindrome or not.c
+++ arrays/pallindrome or not.c
@@ -2,18 +2,17 @@
 //  pallindrome or not.c
 //  Arrays
 //
-//  Created by Gokul Nandakumar on 07/03/23.
-//
+//  Created by Gokul Nandakumar
 
-//#include <stdio.h>
-//int main(){
-//    int a[]={1,2,3,4,5,5,4,3,2,1};
-//    int counter=0;
-//    for (int i=0, j=9; i<10, j>=0; i++, j--){
-//        if(a[i]==a[j]) continue;
-//        else {counter=1; break;}
-//    }
-//    if(counter==1) printf("No");
-//    else printf("Yes");
-//}
-//
+#include <stdio.h>
+int main(){
+   int a[]={1,2,3,4,5,5,4,3,2,1};
+   int counter=0;
+   for (int i=0, j=9; i<10, j>=0; i++, j--){
+       if(a[i]==a[j]) continue;
+       else {counter=1; break;}
+   }
+   if(counter==1) printf("No");
+   else printf("Yes");
+}
+
