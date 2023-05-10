618dcc7d5a64b52bcc8fb333489dc74d81c6ef06
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-05-10 22:45:06 +0530

    Update if used in array question 2.c

--- arrays/if used in array question 2.c
+++ arrays/if used in array question 2.c
@@ -2,17 +2,16 @@
 //  if used in array question 2.c
 //  Arrays
 //
-//  Created by Gokul Nandakumar on 07/03/23.
-//
+//  Created by Gokul Nandakumar 
 
-//#include <stdio.h>
-////difference between sum of even indexed numbers and sum of odd odd indexed numbers
-//int main(){
-//    int arr[]={1,2,3,4,5,6,7,8,9,10};
-//    int even=0;int odd=0;
-//    for (int i=0; i<10; i++) {
-//        if (i%2==0) even+=arr[i];
-//        else  odd+=arr[i];
-//    }
-//    printf("%d", even-odd);
-//}
+#include <stdio.h>
+//difference between sum of even indexed numbers and sum of odd odd indexed numbers
+int main(){
+   int arr[]={1,2,3,4,5,6,7,8,9,10};
+   int even=0;int odd=0;
+   for (int i=0; i<10; i++) {
+       if (i%2==0) even+=arr[i];
+       else  odd+=arr[i];
+   }
+   printf("%d", even-odd);
+}
