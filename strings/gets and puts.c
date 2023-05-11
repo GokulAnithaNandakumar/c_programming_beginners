832dc3b39734a11943799e063f2dd7693a7b86f1
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-05-12 00:17:23 +0530

    Add files via upload

---
+++ strings/gets and puts.c
@@ -0,0 +1,23 @@
+//
+//  gets and puts.c
+//  strings
+//
+//  Created by Gokul Nandakumar on 13/03/23.
+//
+
+//#include<stdio.h>
+//#include <string.h>
+//# define MAX 20
+//int main(){
+//    char str[]="Gokul is good";
+//    printf("%s\n", str);
+//    puts(str);  //printing string
+//    char a[200];
+////    scanf("%s", a);   //HERE DONT USE & !!!!!!   Only the first word will be considered
+////    puts(a);
+//    char b[MAX];
+////    fgets(b, MAX, stdin);  //we can also use fgets(b, 20, stdin);
+////    printf("\n%s", b);
+//    scanf("%[^\n]s", a);   //We can also use this for space string
+//    puts(a);
+//}
