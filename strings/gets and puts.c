b6291ae353ff3b71642f6ab705686f6b207106a0
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-05-12 00:17:43 +0530

    Update gets and puts.c

--- strings/gets and puts.c
+++ strings/gets and puts.c
@@ -2,22 +2,21 @@
 //  gets and puts.c
 //  strings
 //
-//  Created by Gokul Nandakumar on 13/03/23.
-//
+//  Created by Gokul Nandakumar
 
-//#include<stdio.h>
-//#include <string.h>
-//# define MAX 20
-//int main(){
-//    char str[]="Gokul is good";
-//    printf("%s\n", str);
-//    puts(str);  //printing string
-//    char a[200];
-////    scanf("%s", a);   //HERE DONT USE & !!!!!!   Only the first word will be considered
-////    puts(a);
-//    char b[MAX];
-////    fgets(b, MAX, stdin);  //we can also use fgets(b, 20, stdin);
-////    printf("\n%s", b);
-//    scanf("%[^\n]s", a);   //We can also use this for space string
+#include<stdio.h>
+#include <string.h>
+# define MAX 20
+int main(){
+   char str[]="Gokul is good";
+   printf("%s\n", str);
+   puts(str);  //printing string
+   char a[200];
+//    scanf("%s", a);   //HERE DONT USE & !!!!!!   Only the first word will be considered
 //    puts(a);
-//}
+   char b[MAX];
+//    fgets(b, MAX, stdin);  //we can also use fgets(b, 20, stdin);
+//    printf("\n%s", b);
+   scanf("%[^\n]s", a);   //We can also use this for space string
+   puts(a);
+}
