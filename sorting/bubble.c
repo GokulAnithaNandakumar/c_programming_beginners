c1d6578ab6cde43ca8afa2a50ee396e9af4d5c5e
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-05-27 22:27:13 +0530

    Add files via upload

---
+++ sorting/bubble.c
@@ -0,0 +1,35 @@
+////
+////  bubble.c
+////  sorting
+////
+////  Created by Gokul Nandakumar on 24/05/23.
+////
+////
+//#include <stdio.h>
+//
+//void bs(int a[], int n){
+//    int temp, flag;
+//    for(int i=0; i<n-1; i++){
+//        flag=1;
+//        for(int j=0; j<n-1-i; j++){
+//            if(a[j]>a[j+1]){
+//                temp=a[j];
+//                a[j]=a[j+1];
+//                a[j+1]=temp;
+//                flag=0;
+//            }
+//        }
+//        if(flag==1){
+//            break;
+//        }
+//    }
+//}
+//
+//
+//int main(void){
+//    int arr[]={1,5,2,5,6,34,45,65,2,76,243,7};
+//    bs(arr, 12);
+//    for(int i=0; i<12; i++){
+//        printf("%d ", arr[i]);
+//    }
+//}
